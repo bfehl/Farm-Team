d6f69e2f10bcf4913c034dd11570ad386993ed3a
jwolf0 <79229362+jwolf0@users.noreply.github.com>
2021-04-13 18:34:48 -0400

    Create ball_track_follow.py

---
+++ ball_track_follow.py
@@ -0,0 +1,163 @@
+# import the necessary packages
+from collections import deque
+import serial
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+
+
+# define the lower and upper boundaries
+# ball in the HSV color space, then initialize the
+# list of tracked points
+
+lower = (76, 63, 83)
+upper = (101, 168, 176)
+buffer = 20
+pts = deque(maxlen=buffer)
+
+#save real object rad. for distance calc3
+#real_radius = 2.5in/2
+#we need in px
+real_radius = 240/2
+
+
+#start cam & give time to warm up
+camSet='nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv flip-method=2 ! video/x-raw, width=800, height=600, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam = cv2.VideoCapture(camSet)
+time.sleep(2)
+
+#create list for dist. averaging
+#d = deque(maxlen=10)
+
+
+#start serial comms
+robot = serial.Serial('/dev/ttyUSB0', 9600)
+
+counter = 0
+while True:
+    _, frame = cam.read()
+
+    # resize the frame, blur it, and convert it to the HSV
+	# color space
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color "blue", then perform
+	# a series of dilations and erosions to remove any small
+	# blobs left in the mask
+    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+	# (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+
+    
+    	# only proceed if at least one contour was found
+    if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        #calculate and print distance
+        
+        #1in = 96px
+        #at 8inches, ball interperated radius is 65px. 8inch = 768px
+        #cam_interpreted_size = real_size*focal_len/dist
+        #cam_interpreted * dist / realsize = focal_len
+        #focal_len = (65 * 768)/real_radius
+        #I exeperimentally got 416px
+
+        focal_len = 416
+        dist = (real_radius * focal_len)/radius
+        inches = dist/96
+        print(inches, "inches") #This is super rough since the enclosing circle isnt always perfect, but I dont think theres really a fix for that
+
+
+        # only proceed if the radius meets a minimum size
+        if radius > 5:
+			# draw the circle and centroid on the frame
+			# then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+            '''
+            Drive based off of distance, and where in the screen reigon the ball is
+            waits until at least 10 loops have been completed so that theres actually
+            time to perform inputs
+            '''
+
+            if counter >=8:
+                #screen width = 600
+                #if x is in center 3/5, go forward
+                if inches <= 12:
+                    robot.write(b'0')
+                    print("ball captured")
+
+                elif x >= 120 and x <= 480:
+                    robot.write(b'1')
+                    print("forward")
+                    
+                    #pass
+                #if to left 5th, turn left
+                elif x < 120:
+                    robot.write(b'2')
+                    print('left')
+
+                    #pass
+                #if to the right, turn right
+                elif x < 480:
+                    robot.write(b'3')
+                    print('right')
+                    #pass
+                
+                    
+                counter=0
+
+        #if radius < boundary
+        else:
+            #if no ball, stop
+            robot.write(b'0')
+            print('STOP radius doesnt exist')
+            #pass
+
+    counter+=1
+    print(counter)
+
+	# update the points queue
+    pts.appendleft(center)
+
+    	# loop over the set of tracked points
+    for i in range(1, len(pts)):
+		# if either of the tracked points are None, ignore
+		# them
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+		# otherwise, compute the thickness of the line and
+		# draw the connecting lines
+        thickness = int(np.sqrt(buffer / float(i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+	# show the frame to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+
+
+    #press q to quit
+    if cv2.waitKey(1)==ord('q'):
+        robot.write(b'0')
+        cam.release()
+        cv2.destroyAllWindows()
+        
